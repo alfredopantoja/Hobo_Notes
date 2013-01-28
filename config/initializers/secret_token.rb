7728d0bd9cd0c6b36fbbd3d86a32c7f8b24a7f84
dobabeswe <chololoy@gmail.com>
2013-01-28 06:40:13 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoboNotes::Application.config.secret_token = 'a0965006c9a498eb9c4c2d389d06c51aa71c181be0e8fba9dae2b2ea0e369154a479d69cb78838dac2326825c568f0c2eaad4908a08c1f08348c45d4943b4a2c'
