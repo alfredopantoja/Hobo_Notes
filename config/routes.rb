3aa89f26a547f79d471cd1f8e905f65f5e338c7f
dobabeswe <chololoy@gmail.com>
2013-01-30 03:43:30 -0600

    Modify routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 HoboNotes::Application.routes.draw do
-  devise_for :users
-
+  authenticated :user do
+    root to: 'static_pages#home'
+  end    
   root to: 'static_pages#home'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+
+  devise_for :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
