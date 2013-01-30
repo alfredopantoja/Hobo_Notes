23fb5ca9023fe626246dda1840293fc43c1fe30f
dobabeswe <chololoy@gmail.com>
2013-01-30 09:54:30 -0600

    Create Users controller, add authorization and modify home page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ HoboNotes::Application.routes.draw do
   match '/contact', to: 'static_pages#contact'
 
   devise_for :users
+  resources  :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
